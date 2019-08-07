009eff64064b046c926c8f648e0e2eb2b41d20d7
Melvin Peepers <peepersm@gmail.com>
2019-08-06 22:08:36 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+
+// libraries
+const express = require("express");
+
+// global objects
+const server = express();
+
+// request handler
+server.get("/", (req, res) => {
+  res.send("<h1>Challenge</h1>");
+});
+
+// should be last step
+server.listen(4444, () => {
+  console.log("Server is running on port 4444...");
+});

793c064c948c9ee28fe295979b427bc1b61c06f2
Melvin Peepers <peepersm@gmail.com>
2019-08-07 21:27:01 -0400

    added POST

--- index.js
+++ index.js
@@ -20,14 +20,43 @@ server.get("/", (req, res) => {
 server.get("/api/users", (req, res) => {
   db.find()
     .then(users => {
+      // console.log("users", users);
       res.json(users);
     })
     .catch(error => {
       res.status(500).json({
-        error: error
+        error: "The users information could not be retrieved."
       });
     });
 });
+// tested the above GET and it works
+
+// GET user ID
+
+// POST user
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+
+  db.insert(newUser)
+    .then(user => {
+      if (newUser.name && newUser.bio) {
+        res.status(201).json(user);
+      } else {
+        res.status(400).json({
+          errorMessage: "Please provide name and bio for the user."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
+// DELETE user
+
+// PUT user
 
 // should be last step
 server.listen(4444, () => {

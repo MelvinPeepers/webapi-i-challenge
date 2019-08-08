eb4fe3bfc57e435b98a02a860862955247214ca5
Melvin Peepers <peepersm@gmail.com>
2019-08-07 20:28:17 -0400

    setup GET user

--- index.js
+++ index.js
@@ -3,12 +3,30 @@
 // libraries
 const express = require("express");
 
+// other files
+const db = require("./data/db.js");
+
 // global objects
 const server = express();
 
+// What happens on a GET request to /
 // request handler
+// req (request) is what is coming to us, res (response) is what we're sending out
 server.get("/", (req, res) => {
-  res.send("<h1>Challenge</h1>");
+  res.send("<h3> Web API Challenge</h3>");
+});
+
+// GET users
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error
+      });
+    });
 });
 
 // should be last step
